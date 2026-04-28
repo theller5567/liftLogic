9b6192193e78be728a998cb300b39de4c6a582f5
Travis <theller5567@gmail.com>
2026-04-28 11:08:21 -0400

    file name issue

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './app.css'
+import './app.scss'
 import './styles/font-utilities.scss'
 import App from './App.tsx'
 
